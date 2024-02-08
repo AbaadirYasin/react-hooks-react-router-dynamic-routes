1346d73d3ebaf34d5c907a695f46d87b2193310d
AbaadirYasin <abaadir.yasin@student.moringaschool.com>
2024-02-08 12:54:36 +0000

    completed

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -10,3 +10,27 @@ function MoviesPage({ movies }) {
   );
 }
 export default MoviesPage;
+import React from "react";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
+
+function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
+  return (
+    <div>
+      <MoviesList movies={movies} />
+
+       <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
+    </div>
+  );
+
+}
+export default MoviesPage;
